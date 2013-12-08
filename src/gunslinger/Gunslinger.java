0e9b07ba67004f743ee00873dbfe1086311614cc
Justin Wang <hzjustin@hotmail.com>
2013-12-07 18:07:07 -0800

    Added skeleton main function.

---
+++ src/gunslinger/Gunslinger.java
@@ -0,0 +1,14 @@
+package gunslinger;
+
+/**
+ * Main function for the game.
+ * 
+ * @author Justin Wang
+ *
+ */
+public class Gunslinger {
+	
+	public static void main( String[] args ){
+		
+	}
+}
